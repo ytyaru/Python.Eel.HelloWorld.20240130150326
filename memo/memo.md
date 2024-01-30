93ca5803c921024d408ad92ac06ea160841d2201
ytyaru <yttry0@gmail.com>
2024-01-30 21:46:43 +0900

    add 5

--- memo/memo.md
+++ memo/memo.md
@@ -28,3 +28,20 @@ cd $NAME/src
 # coding: utf8
 ```
 
+* [PythonでEelを使ってUbuntuのGUIを開発する方法][]
+
+[PythonでEelを使ってUbuntuのGUIを開発する方法]:https://zenn.dev/chaomiosoft/scraps/879d9ebdb80a06
+
+```python
+def gui_init():
+  eel.init("web")
+  eel.start("index.html", port=8080)
+
+if __name__ == '__main__':
+  gui_process = Process(target=gui_init)
+  gui_process.start()
+```
+```python
+eel.start("index.html", port=8080, cmdline_args=['--kiosk', '--disable-gpu'])
+```
+
---
+++ src/5/web/js/main.js
@@ -0,0 +1,11 @@
+window.addEventListener('DOMContentLoaded', async(event) => {
+    const {div,span,h1,h2,p,br,em,ruby,rt,rp,textarea,button,input,table,tr,th,td,pre} = van.tags
+    /*
+    van.add(document.body, 
+        h1(a({href:'https://github.com/python-eel/Eel'}, 'Eel'), 'と', a({href:'https://vanjs.org/'}, 'VanJS'), 'でアプリ開発'),
+        input({id:'name',placeholder:'任意のテキスト'}),
+        button({id:'save', onclick:e=>eel.save(document.querySelector('#name').value)}, '保存'))
+    */
+
+})
+
